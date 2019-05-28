deeb32e820e830e90fbeaef45466c02dffdf3d29
HeshamMagdy97 <hesham_magdy97@hotmail.com>
2019-05-28 21:31:18 +0200

    more validations

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -2,7 +2,17 @@ class Todo < ApplicationRecord
   belongs_to :user
   validates :priority, inclusion: 1..5
   validates_presence_of :priority, :title, :description, :user_id, :todo_type
+  validates :todo_type,
+            inclusion: { in: %w[task event],
+                         message: "#{value} is not a valid type" }
 
+  validate :valide_due_date
+
+  def happened_at_is_valid_datetime
+    if due_date.present?
+      errors.add(:due_date, 'must be a valid datetime it cant be at past') if (due_date < Date.today)
+    end
+  end
 ## add location
 
   def self.user_todos(user_id)
